3650c93c42720ad6efc3bcfbdc0467bcb6d48bd5
Francisco Ramos <blbramos@gmail.com>
2022-01-09 20:22:29 -0500

    .13

---
+++ Assets/Scripts/Enemy/BossComponentAuthoring.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+[InternalBufferCapacity(8)]
+
+[Serializable]
+public struct BossWaypointBufferElement : IBufferElementData
+{
+    public float3 wayPointPosition;
+}
+
+
+
+
+public class BossComponentAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+
+    //public bool matchupClosest = true;
+    //public bool leader = false;
+
+    //public float AngleRadians = 180;
+    //public float ViewDistanceSQ = 100;
+
+    //public bool View360 = false;
+
+    public List<WayPoint> wayPoints = new List<WayPoint>();
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+
+        dstManager.AddBuffer<BossWaypointBufferElement>(entity).Add
+            (
+                new BossWaypointBufferElement 
+                { 
+                    wayPointPosition = wayPoints[0].targetPosition 
+                }
+
+            );
+
+
+    }
+}
