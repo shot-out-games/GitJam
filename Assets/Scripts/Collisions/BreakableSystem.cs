bd41837606ed5f4cc9a77adf1d44ead95c334c1e
Francisco Ramos <blbramos@gmail.com>
2022-01-23 14:24:27 -0500

    .43

--- Assets/Scripts/Collisions/BreakableSystem.cs
+++ Assets/Scripts/Collisions/BreakableSystem.cs
@@ -34,81 +34,81 @@ public class BreakableSystem : SystemBase
 
         ) =>
         {
-            //if (dead.isDead == true) return;
+            if (dead.isDead == true) return;
 
 
-            //int type_a = collisionComponent.Part_entity;
-            //int type_b = collisionComponent.Part_other_entity;
-            //Entity collision_entity_a = collisionComponent.Character_entity;
-            //Entity collision_entity_b = collisionComponent.Character_other_entity;
-
-
-
-            //Debug.Log("breakable a " + collision_entity_a + " type a " + type_a);
-            //Debug.Log("breakable b " + collision_entity_b + " type a " + type_b);
+            int type_a = collisionComponent.Part_entity;
+            int type_b = collisionComponent.Part_other_entity;
+            Entity collision_entity_a = collisionComponent.Character_entity;
+            Entity collision_entity_b = collisionComponent.Character_other_entity;
 
 
 
-            //if (type_b == (int)TriggerType.Breakable && HasComponent<TriggerComponent>(collision_entity_a)
-            //                                 && HasComponent<TriggerComponent>(collision_entity_b)) //b is damage effect so causes damage to entity
-            //{
+            Debug.Log("breakable a " + collision_entity_a + " type a " + type_a);
+            Debug.Log("breakable b " + collision_entity_b + " type a " + type_b);
 
 
 
-            //    var breakableComponent = GetComponent<BreakableComponent>(collision_entity_b);
-
-            //    float damage = 0;
-            //    int effectsIndex = 0;
-
-            //    bool skip = false;
-            //    //if (visualEffectComponent.frameSkipCounter < visualEffectComponent.framesToSkip)
-            //    if (breakableComponent.frameSkipCounter == 0)
-            //    {
-            //        breakableComponent.frameSkipCounter += 1;
-            //        skip = false;
-            //    }
-            //    else if (breakableComponent.frameSkipCounter <  breakableComponent.framesToSkip)
-
-            //    {
-            //        breakableComponent.frameSkipCounter += 1;
-            //        skip = true;
-            //    }
-            //    else if (breakableComponent.frameSkipCounter >= breakableComponent.framesToSkip)
-
-            //    {
-            //        breakableComponent.frameSkipCounter = 0;
-            //        skip = true;
-            //    }
-
-            //    if (skip == false)
-            //    {
-            //        damage = breakableComponent.damageAmount;
-            //        //effectsIndex = (int)EffectType.Damaged;
-            //        effectsIndex = breakableComponent.damageEffectsIndex;//???
-            //    }
-            //    Debug.Log("breakable damage " + damage);
-
-            //    if (HasComponent<DeadComponent>(collision_entity_a) == false ||
-            //        GetComponent<DeadComponent>(collision_entity_a).isDead)
-            //    {
-            //        damage = 0;
-            //    }
+            if (type_b == (int)TriggerType.Breakable && HasComponent<TriggerComponent>(collision_entity_a)
+                                             && HasComponent<TriggerComponent>(collision_entity_b)) //b is damage effect so causes damage to entity
+            {
 
 
 
+                var breakableComponent = GetComponent<BreakableComponent>(collision_entity_b);
 
+                float damage = 0;
+                int effectsIndex = 0;
 
-            //    ecb.AddComponent<DamageComponent>(collision_entity_a,
-            //            new DamageComponent
-            //            { DamageLanded = 0, DamageReceived = damage, StunLanded = damage, entityCausingDamage = collision_entity_b, effectsIndex = effectsIndex });
+                bool skip = false;
+                //if (visualEffectComponent.frameSkipCounter < visualEffectComponent.framesToSkip)
+                if (breakableComponent.frameSkipCounter == 0)
+                {
+                    breakableComponent.frameSkipCounter += 1;
+                    skip = false;
+                }
+                else if (breakableComponent.frameSkipCounter <  breakableComponent.framesToSkip)
+
+                {
+                    breakableComponent.frameSkipCounter += 1;
+                    skip = true;
+                }
+                else if (breakableComponent.frameSkipCounter >= breakableComponent.framesToSkip)
+
+                {
+                    breakableComponent.frameSkipCounter = 0;
+                    skip = true;
+                }
+
+                if (skip == false)
+                {
+                    damage = breakableComponent.damageAmount;
+                    //effectsIndex = (int)EffectType.Damaged;
+                    effectsIndex = breakableComponent.damageEffectsIndex;//???
+                }
+                Debug.Log("breakable damage " + damage);
+
+                if (HasComponent<DeadComponent>(collision_entity_a) == false ||
+                    GetComponent<DeadComponent>(collision_entity_a).isDead)
+                {
+                    damage = 0;
+                }
 
 
 
 
-            //    ecb.SetComponent<BreakableComponent>(collision_entity_b, breakableComponent);
+
+                ecb.AddComponent<DamageComponent>(collision_entity_a,
+                        new DamageComponent
+                        { DamageLanded = 0, DamageReceived = damage, StunLanded = damage, entityCausingDamage = collision_entity_b, effectsIndex = effectsIndex });
 
 
-            //}
+
+
+                ecb.SetComponent<BreakableComponent>(collision_entity_b, breakableComponent);
+
+
+            }
 
 
 
