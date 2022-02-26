e436dae7312208d5a03b2eb9722ca44b0859bfaf
Francisco Ramos <blbramos@gmail.com>
2022-02-25 20:45:40 -0500

    .9912

---
+++ Assets/Levels/Themes/Rules/SpacePropRandomRotZ.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DungeonArchitect;
+
+public class SpacePropRandomRotZ : TransformationRule {
+
+    public override void GetTransform(PropSocket socket, DungeonModel model, Matrix4x4 propTransform, System.Random random, out Vector3 outPosition, out Quaternion outRotation, out Vector3 outScale)
+    {
+        outPosition = Vector3.zero;
+        outRotation = Quaternion.Euler(0, random.NextFloat() * 360, 0);
+        outScale = Vector3.one;
+    }
+}
